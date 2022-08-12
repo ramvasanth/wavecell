7f727fb3d12f8cef12db847cea56fa30bdd1477e
fairyhunter13 <fairyhunter13@gmail.com>
2022-08-12 09:41:43 +0700

    - Add callback.go :sparkles:

---
+++ callback.go
@@ -0,0 +1,43 @@
+package wavecell
+
+import (
+	"github.com/shopspring/decimal"
+)
+
+// CallbackData specifies the callback data submitted by the vendor.
+type CallbackData struct {
+	Namespace   string              `json:"namespace"`
+	EventType   string              `json:"eventType"`
+	Description string              `json:"description"`
+	Payload     CallbackDataPayload `json:"payload"`
+}
+
+// CallbackDataPayload specifies the message payload of callback data submitted by the vendor.
+type CallbackDataPayload struct {
+	UMID            string        `json:"umid"`
+	BatchID         string        `json:"batchId"`
+	ClientMessageID string        `json:"clientMessageId"`
+	ClientBatchID   string        `json:"clientBatchId"`
+	SubAccountID    string        `json:"subAccountId"`
+	Source          string        `json:"source"`
+	Destination     string        `json:"destination"`
+	Status          MessageStatus `json:"status"`
+	Price           MessagePrice  `json:"price"`
+	SmsCount        int           `json:"smsCount"`
+}
+
+// MessageStatus specifies the message status.
+type MessageStatus struct {
+	State        string `json:"state"`
+	Detail       string `json:"detail"`
+	Timestamp    string `json:"timestamp"`
+	ErrorCode    int    `json:"errorCode"`
+	ErrorMessage string `json:"errorMessage"`
+}
+
+// MessagePrice specifies the message price.
+type MessagePrice struct {
+	Total    *decimal.Decimal `json:"total"`
+	PerSMS   decimal.Decimal  `json:"perSms"`
+	Currency *string          `json:"currency"`
+}
--- go.mod
+++ go.mod
@@ -10,5 +10,6 @@ require (
 	github.com/gojek/heimdall/v7 v7.0.2
 	github.com/jarcoal/httpmock v1.2.0
 	github.com/pkg/errors v0.9.1
+	github.com/shopspring/decimal v1.3.1
 	github.com/stretchr/testify v1.7.1
 )
